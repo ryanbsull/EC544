7476104ac655ae371214237f8bd9ca0d45436cda
ryanbs183 <ryan.sullivan.183@gmail.com>
2022-04-06 16:17:13 -0400

    added in client side as well as ability to handle multiple clients

--- pi/server.c
+++ pi/server.c
@@ -8,9 +8,11 @@
 
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket, valread;
+    int server_fd, valread;
+    int new_socket[3];
     struct sockaddr_in address;
     int opt = 1;
+    int numclient = 0;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
     char *hello = "Hello from team 9";
@@ -22,8 +24,8 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
        
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+    // Forcefully attaching socket to the port
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
                                                   &opt, sizeof(opt)))
     {
         perror("setsockopt");
@@ -33,7 +35,7 @@ int main(int argc, char const *argv[])
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
        
-    // Forcefully attaching socket to the port 8080
+    // Forcefully attaching socket to the port
     if (bind(server_fd, (struct sockaddr *)&address, 
                                  sizeof(address))<0)
     {
@@ -45,15 +47,18 @@ int main(int argc, char const *argv[])
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
+    while(numclient < 3) {
+        if ((new_socket[numclient] = accept(server_fd, (struct sockaddr *)&address, 
+                        (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        valread = read( new_socket[numclient] , buffer, 1024);
+        printf("%s\n",buffer );
+        send(new_socket[numclient] , hello , strlen(hello) , 0 );
+        printf("Hello message sent\n");
+        numclient++;
     }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
     return 0;
 }
