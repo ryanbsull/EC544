566b1be3f75176fd850ada533e23ff84ea848654
Maniadarsh Sivaram <maniadarshsivaram@Maniadarshs-MacBook-Pro.local>
2022-04-25 22:36:46 -0400

    DHT 22 Sensor integrated

--- Sensor Integration/dht_example.py
+++ Sensor Integration/dht_example.py
@@ -1,12 +1,7 @@
 import time
 import board
 import adafruit_dht
-# Initial the dht device, with data pin connected to:
-# dhtDevice = adafruit_dht.DHT22(board.D4)
 
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
 dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 while True:
     try:
@@ -21,12 +16,11 @@ while True:
         )
 
     except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
         time.sleep(2.0)
         continue
     except Exception as error:
         dhtDevice.exit()
         raise error
- 
+
     time.sleep(2.0)
